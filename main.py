6d0a12dac9641734861cdf441a01d29e2d1e96f0
joycedfan <joycedfan@gmail.com>
2017-10-08 17:44:48 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+from room import Room
+
+kitchen = Room("Kitchen")
+kitchen.set_description("A dank and dirty room buzzing with flies.")
+
+dining_hall = Room("Dining Hall")
+dining_hall.set_description("A large room with ornate golden decorations on every wall.")
+
+ballroom = Room("Ballroom")
+ballroom.set_description("A vast room with a shiny wooden floor. Huge candlesticks guard the entrance.")
+
+kitchen.link_room(dining_hall, "south")
+dining_hall.link_room(kitchen, "north")
+dining_hall.link_room(ballroom, "west")
+ballroom.link_room(dining_hall, "east")
+
+current_room = kitchen
+
+while True:
+    print ("\n")
+    current_room.get_details()
+    command = raw_input("> ")
+    current_room = current_room.move(command)
+
