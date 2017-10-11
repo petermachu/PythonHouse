d15f46e23e7be679c1df456c706d2faf920039f5
petermachu <32624166+petermachu@users.noreply.github.com>
2017-10-10 22:59:21 -0700

    Update main.py

--- main.py
+++ main.py
@@ -9,6 +9,9 @@ dining_hall.set_description("A large room with ornate golden decorations on ever
 ballroom = Room("Ballroom")
 ballroom.set_description("A vast room with a shiny wooden floor. Huge candlesticks guard the entrance.")
 
+closet = Room("Closet")
+closet.set_description("A small closet filled with cleaning supplies.")
+
 hallway = Room("Hallway")
 hallway.set_description ("A long hallway lined with paintings."
 
@@ -17,17 +20,24 @@ cellar.set_description ("A dark smelly room littered with rotting food that emit
 
 wine_cellar = Room("Wine Cellar")
 wine_cellar.set_description ("A nearly pitch black room with empty wineracks along the walls and broken glass on the floor.")
-
+                       
+pool_room = Room("Pool Room")
+pool_room.set_description("A big bright room with red carpet with spilled orange juice on it.")
+                         
 kitchen.link_room(dining_hall, "south")
 dining_hall.link_room(kitchen, "north")
 dining_hall.link_room(ballroom, "west")
 ballroom.link_room(dining_hall, "east")
 hallway.link_room(ballroom, "south")
 ballroom.link_room(hallway, "north")
+hallway.link_room(closet, "west")
+closet.link_room(hallway, "east")
 cellar.link_room(kitchen, "up")
 kitchen.link_room(cellar, "below")
 cellar.link_room(wine_cellar, "east")
 wine_cellar.link_room(cellar, "west")
+pool_room.link_room(ballroom, "below")
+ballroom.link_room(pool_room, "up")
                          
 current_room = kitchen
 
